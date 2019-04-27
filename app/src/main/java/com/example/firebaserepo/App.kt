e628879b477650f36b82750198a693c12c16f483
Sanket <vetkolisanket@gmail.com>
2019-04-27 21:54:10 +0530

    Added facebook login via firebase

---
+++ app/src/main/java/com/example/firebaserepo/App.kt
@@ -0,0 +1,16 @@
+package com.example.firebaserepo
+
+import android.app.Application
+
+/**
+ * Created by Sanket on 27/04/19.
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        FacebookSdk.sdkInitialize(applicationContext)
+//        AppEventsLogger.activateApp(this)
+    }
+
+}
---
+++ app/src/main/java/com/example/firebaserepo/features/login/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.firebaserepo.features.login
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.example.firebaserepo.R
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private val callbackManager = CallbackManager.Factory.create()!!
+
+    val TAG = LoginActivity::class.java.simpleName
+
+    companion object {
+        @JvmStatic
+        fun newIntent(context: Context) = Intent(context, LoginActivity::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        val EMAIL = "email"
+        val PUBLIC_PROFILE = "public_profile"
+
+        loginButton.setReadPermissions(Arrays.asList(EMAIL, PUBLIC_PROFILE))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                Toast.makeText(this@LoginActivity, "Success", Toast.LENGTH_SHORT).show()
+//                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Toast.makeText(this@LoginActivity, "Cancel", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(exception: FacebookException) {
+                Toast.makeText(this@LoginActivity, "Error", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
